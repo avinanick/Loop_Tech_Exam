1dff45b2da53fc2c17000e36c212818c7b6d5134
avinanick <avinanick@gmail.com>
2024-06-30 17:48:35 -0700

    Increased timeout due to tests exceeding time taken.

--- playwright.config.js
+++ playwright.config.js
@@ -16,6 +16,8 @@ module.exports = defineConfig({
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
+  /* Increasing timeout so tests can run to completion */
+  timeout: 5 * 60 * 1000,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
